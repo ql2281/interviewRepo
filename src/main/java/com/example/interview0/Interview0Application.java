b5848a900dead04535782af314b72432a1ca9555
Hey <man@Heys-MacBook-Pro.local>
2019-05-13 15:11:11 -0700

    first commit

---
+++ src/main/java/com/example/interview0/Interview0Application.java
@@ -0,0 +1,13 @@
+package com.example.interview0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interview0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Interview0Application.class, args);
+    }
+
+}
